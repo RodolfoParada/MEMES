b0c47a9b15cd1c70c27f2b1b17cccf2e314aa98a
Rodolfo <73254069+RodolfoParada@users.noreply.github.com>
2023-02-16 22:55:27 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+ <ListMemes />
+  </div>
+</template>
+
+<script>
+import ListMemes from './components/ListMemes.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+     ListMemes,
+  },
+
+  
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Meme.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="col-3 memes">Meme</div>
+  <p>{{ meme.name }}</p>
+  <img :src="meme.url" :alt="name" class="fluid" />
+</template>
+
+<script>
+
+
+export default {
+  props: {
+    meme: Object,
+  }
+}
+</script>
+
+<style scoped>
+
+.meme{
+    height: 300px;
+    display: flex;
+    flex-direction: column;
+    align-items:center;
+    border: 1px solid grey;
+
+}
+
+.meme img {
+    max-height: 200px;
+    max-width: 250px;
+}
+</style>
